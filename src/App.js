f7b7ba557071afa32d7f03c3f030974775098072
Paula del Castillo Vivero <paulaDelcastillovivero@gmail.com>
2021-08-19 22:06:29 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import  {useSpring,animated} from "react-spring";
+import {Navbar} from "./Components/Navbar";
 function App() {
+  const props = useSpring({to: {opacity:1}, from: {opacity:0  }})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="min-h-screen bg-gray-900">
+        <Navbar/>
+
+        <animated.div style ={props}className="App">
+          I will fade in
+        </animated.div>
+      </div>
   );
+
 }
 
 export default App;
